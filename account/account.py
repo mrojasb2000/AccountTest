cbbd63f665a2e3c3034a7bd2d6f1dc2c9b614236
Mauricio Rojas <mrojasb2000@gmail.com>
2023-03-10 22:27:29 -0300

    chore: add test balance greater than zero

--- account/account.py
+++ account/account.py
@@ -6,4 +6,13 @@ class Account:
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
+
+    def debit(self, a):
+        if self.balance < a:
+          return "Error: Raised when the balance less or equal than zero"
+        else:
+            self.balance -= a
+            return self.balance
+            
+               
    
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,8 +1,28 @@
 from account.account import Account
+
 import pytest
 
-def test_whenCreateAccountThenReturnAccountInstance():
+
+def test_when_create_account_then_return_account_instance():
     account = Account("Mavro", 1000)
     expected = 1000
+    
     actual = account.balance
+    
     assert actual == expected
+
+def test_should_throw_exception_when_debit_insufficient_balance():
+     account = Account("Mavro", 1000)
+     expected = "Error: Raised when the balance less or equal than zero"
+
+     actual = account.debit(1500)
+
+     assert actual == expected
+
+def test_should_return_balance_when_debit_greater_than_zero():
+     account = Account("Mavro", 1000)
+     expected = 500
+
+     actual = account.debit(500)
+     
+     assert actual == expected     
