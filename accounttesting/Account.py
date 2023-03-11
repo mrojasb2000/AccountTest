d96ff1d3825397b7f61a035c212b1e5f5c4fc1dd
Mauricio Rojas <mrojasb2000@gmail.com>
2023-03-10 21:19:42 -0300

    chore: refactor account test

---
+++ accounttesting/Account.py
@@ -0,0 +1,5 @@
+class Account:
+    # Python provides support to generic number of arguments 
+    # (variadic functions)
+    def add(self, name, balance):
+        pass
--- tests/test_accounttesting.py
+++
@@ -1,4 +0,0 @@
-import pytest
-
-def test_add_many_numbers():
-    assert True == True
